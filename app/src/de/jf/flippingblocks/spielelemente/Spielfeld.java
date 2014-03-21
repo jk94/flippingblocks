11d13fa30fe0e562911a94a4d7cd56dcd9ea0072
Jan <jankoschke@googlemail.com>
2014-03-21 10:21:04 +0100

    Spielfeld weiterentwickelt

--- app/src/de/jf/flippingblocks/spielelemente/Spielfeld.java
+++ app/src/de/jf/flippingblocks/spielelemente/Spielfeld.java
@@ -2,6 +2,8 @@ package de.jf.flippingblocks.spielelemente;
 
 import java.util.ArrayList;
 
+import de.jf.flippingblocks.graphics.BlockPanel;
+
 public class Spielfeld {
 	
 	ArrayList<ArrayList<String>> array = new ArrayList<ArrayList<String>>();
@@ -17,6 +19,42 @@ public class Spielfeld {
 		}
 	}
 	
+	public Block getBlockByBtnRef(BlockPanel b){
+		Block erg = null;
+		for(ArrayList<Block> y:blocklist){
+			for(Block bl:y){
+				if(bl.getBtn().equals(b)){
+					erg = bl;
+					break;
+				}
+			}
+			if(!erg.equals(null)){
+				break;
+			}
+		}
+		return erg;
+	}
+	
+	public ArrayList<Block> checkMates(Block b){
+		ArrayList<Block> erg = new ArrayList<Block>();
+		int x, y;
+		boolean posfound=false;
+		for(int i=0;i<blocklist.size();i++){
+			for(int i2=0;i2<blocklist.get(i).size();i2++){
+				if(blocklist.get(i).get(i2).equals(b)){
+					x = i;
+					y = i2;
+					posfound = true;
+					break;
+				}
+			}
+			if(posfound){
+				break;
+			}
+		}
+		return erg;
+	}
+
 	
 
 }
