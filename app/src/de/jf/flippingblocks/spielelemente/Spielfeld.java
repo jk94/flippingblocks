9126c1057ad0c08085859f48792bb09a0e1fbbc6
Jan <jankoschke@googlemail.com>
2014-03-23 00:55:50 +0100

    Spielfeld

--- app/src/de/jf/flippingblocks/spielelemente/Spielfeld.java
+++ app/src/de/jf/flippingblocks/spielelemente/Spielfeld.java
@@ -2,6 +2,7 @@ package de.jf.flippingblocks.spielelemente;
 
 import java.util.ArrayList;
 
+import de.jf.flippingblocks.Enum.EnumColor;
 import de.jf.flippingblocks.graphics.BlockPanel;
 
 public class Spielfeld {
@@ -14,21 +15,17 @@ public class Spielfeld {
 		borderX = sizex;
 		borderY = sizey;
 		blocklist = new ArrayList<ArrayList<Block>>(borderX);
-		System.out.println("Listen erstellt!");
-		System.out.println("Listen befüllen!");
 		for (int i = 0; i < borderX; i++) {
 			blocklist.add(new ArrayList<Block>(borderY));
 			for (int t = 0; t < borderY; t++) {
-				int color = (int) (1 + (Math.random() * 13));
-				Block b = new Block(color);
+				int color = (int) (1 + (Math.random() * 4));
+				Block b = new Block(EnumColor.values()[color], null);
 				blocklist.get(i).add(b);
-				System.out.println(i + "/" + t + " " + color);
 			}
 		}
-
 	}
 
-	// finding Block by the Reference of its (clicked) Button
+	//Gibt den Block zurück, zu dem der (geklickte) Button (Referenz) gehört.
 	public Block getBlockByBtnRef(BlockPanel b) {
 		Block erg = null;
 		for (ArrayList<Block> y : blocklist) {
@@ -45,76 +42,111 @@ public class Spielfeld {
 		return erg;
 	}
 
-	// check for the apposing Blocks
-	public ArrayList<Block> checkMates(Block b, Block callingBlock) {
-		ArrayList<Block> erg = new ArrayList<Block>();
-		int x = -1, y = -1;
-		boolean posfound = false;
-		for (int i = 0; i < blocklist.size(); i++) {
-			for (int i2 = 0; i2 < blocklist.get(i).size(); i2++) {
-				if (blocklist.get(i).get(i2).equals(b)) {
-					x = i;
-					y = i2;
-					posfound = true;
-					break;
-				}
+	//Überprüft, ob Block die Richtige Farbe hat (zum abbauen) und fügt ihn in Liste hinzu
+	//Überprüft die anliegenden Blöcke auf passende Farbe -> Rekursiv
+	public ArrayList<Block> CheckMate(ArrayList<Block> ergebnisliste,
+			Block pruefblock, EnumColor color) {
+
+		if (!schonVorhanden(ergebnisliste, pruefblock)
+				&& pruefblock.getColor().equals(color)) {
+			ergebnisliste.add(pruefblock);
+			if (!schonVorhanden(ergebnisliste, getBlockNext(pruefblock, 1, 0))) {
+				CheckMate(ergebnisliste, getBlockNext(pruefblock, 1, 0), color);
 			}
-			if (posfound) {
+			if (!schonVorhanden(ergebnisliste, getBlockNext(pruefblock, -1, 0))) {
+				CheckMate(ergebnisliste, getBlockNext(pruefblock, -1, 0), color);
+			}
+			if (!schonVorhanden(ergebnisliste, getBlockNext(pruefblock, 0, 1))) {
+				CheckMate(ergebnisliste, getBlockNext(pruefblock, 0, 1), color);
+			}
+			if (!schonVorhanden(ergebnisliste, getBlockNext(pruefblock, 0, -1))) {
+				CheckMate(ergebnisliste, getBlockNext(pruefblock, 0, -1), color);
+			}
+		}
+		return ergebnisliste;
+	}
+
+	// Prüft ob Block in einer Liste schon vorhanden -> Verwendung, ob Block zum zerstören schon in der Liste.
+	public boolean schonVorhanden(ArrayList<Block> ergebnisliste,
+			Block pruefblock) {
+		try {
+			pruefblock.getColor();
+		} catch (NullPointerException e) {
+			return true;
+		}
+		boolean schonvorhanden = false;
+		for (Block b : ergebnisliste) {
+			if (b.equals(pruefblock)) {
+				schonvorhanden = true;
 				break;
 			}
 		}
-		ArrayList<Block> temp = new ArrayList<Block>();
+		return schonvorhanden;
+	}
 
-		if (x > 0) {
-			if (BlockAccording(blocklist.get(x - 1).get(y), b, callingBlock)) {
-				temp.add(blocklist.get(x - 1).get(y));
+	// Gibt einen Block in x/y-Richtung vom angegebenen Block an. (Akzeptiert
+	// auch Minuswerte)
+	// Ist die angegebene Position außerhalb des Bereichs gibt es >null< zurück.
+	public Block getBlockNext(Block ausgangsBlock, int xRichtung, int yRichtung) {
+		Block erg = null;
+		// Find Block
+		int x = 0, y = 0;
+		boolean found = false;
+		for (int i = 0; i < blocklist.size(); i++) {
+			for (int n = 0; n < blocklist.get(i).size(); n++) {
+				if (blocklist.get(i).get(n).equals(ausgangsBlock)) {
+					x = i;
+					y = n;
+					found = true;
+					break;
+				}
+			}
+			if (found) {
+				break;
 			}
 		}
-		if (x < borderX) {
-			if (BlockAccording(blocklist.get(x + 1).get(y), b, callingBlock)) {
-				temp.add(blocklist.get(x + 1).get(y));
-			}
+		x = x + xRichtung;
+		y = y + yRichtung;
+		if (x < 0) {
+			x = 0;
 		}
-		if (y > 0) {
-			if (BlockAccording(blocklist.get(x).get(y - 1), b, callingBlock)) {
-				temp.add(blocklist.get(x).get(y - 1));
-			}
+		if (x < blocklist.size() && x >= 0 && y >= 0
+				&& y < blocklist.get(0).size()) {
+			erg = blocklist.get(x).get(y);
+		}
 
-		}
-		if (y < 0) {
-			if (BlockAccording(blocklist.get(x).get(y + 1), b, callingBlock)) {
-				temp.add(blocklist.get(x).get(y + 1));
-			}
-		}
+		return erg;
+	}
 
-		for (Block blo : temp) {
-			ArrayList<Block> temp2 = checkMates(blo, b);
-			for (int i = 0; i < temp2.size(); i++) {
-				for (int i2 = 0; i2 < temp.size(); i2++) {
-					if (temp2.get(i).equals(temp.get(i2))) {
-						temp.remove(i2);
+	// Zerstört die (gefundenen) Blöcke/Bei Spielende könnten alle Blöcke
+	// zerstört werden.
+	// TODO (-> Animation?)
+	public void destroyBlocks(ArrayList<Block> list) {
+		for (Block b : list) {
+			boolean found = false;
+			for (ArrayList<Block> b1 : this.blocklist) {
+				for (Block b2 : b1) {
+					if (b2.equals(b)) {
+						b1.remove(b);
+						break;
 					}
 				}
-
+				if (found) {
+					break;
+				}
 			}
 		}
-
-		return erg;
 	}
 
-	// Check if the Block beside is same Color & not the same which called
-	private boolean BlockAccording(Block newBlock, Block actualBlock,
-			Block calledBlock) {
-		boolean erg = true;
-
-		if (newBlock.getColor() != actualBlock.getColor()) {
-			erg = false;
+	// Füllt das Spielfeld wieder mit Blöcken auf
+	//TODO Referenzen für Buttons auf GUI
+	public void fillWithBlocks() {
+		for (ArrayList<Block> list : blocklist) {
+			while (list.size() < borderY) {
+				int color = (int) (1 + (Math.random() * 4));
+				Block b = new Block(EnumColor.values()[color], null);
+				list.add(b);
+			}
 		}
-		if (newBlock.equals(calledBlock)) {
-			erg = false;
-		}
-
-		return erg;
 	}
-
 }
